1b7e2b7f6dca9c85d8474465bd419844b4d6d322
ashish saini <smartsainiashish1@gmail.com>
2022-02-15 14:38:47 +0530

    encapsulation implemented

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './App.css';
 import { Comp1 } from './components/comp1';
 import RefComp1 from './components/forwarding Ref/refComp1';
 import RefComp2 from './components/forwarding Ref/refComp2';
+import Encapsulation from "./components/classesConcepts/encapsulation";
 
 interface a1 {
   login: "yes" | "no"
@@ -13,7 +14,8 @@ function App() {
   return (
     <div className="App">
       {/* {"yes" === val.login && <Comp1 name="ripper" num={54} tip={5} type="rich" />} */}
-      <RefComp1 />
+      {/* <RefComp1 /> */}
+      <Encapsulation />
     </div>
   );
 }
--- src/components/classesConcepts/encapsulation.tsx
+++ src/components/classesConcepts/encapsulation.tsx
@@ -1,9 +1,58 @@
+/* Encapsulation means restricting the property to be accessed by anyone using a class,
+It can be achieved by using access modifiers like private and getters and setters */
 import React, { Component } from 'react';
+class Encapsulation {
+    private readonly amtInBank: number = 50000;
+    private depositAmt: number = 0;
+    constructor(depositAmt: number) {
+        this.depositAmt = depositAmt;
+    }
+    getAmtInBank = () => { //getter function
+        return this.amtInBank;
+    }
+    setDepositAmt = (depositAmt: number) => {
+        console.log(typeof this.depositAmt, typeof depositAmt);
+        depositAmt ? this.depositAmt = this.depositAmt + depositAmt : this.depositAmt = this.depositAmt
+    }
+    getDepositAmt = () => {
+        return this.depositAmt;
+    }
+}
 
-export class encapsulation extends Component {
-  render() {
-    return <div></div>;
-  }
+interface encapsulationState {
+    depositedAmtState: number
+}
+
+export class encapsulation extends Component<{}, encapsulationState> { //React class
+    constructor(props: any) {
+        super(props)
+        this.state = {
+            depositedAmtState: 1,
+        }
+    }
+    render() {
+        const inputRf = React.createRef<any>()
+        const bank = new Encapsulation(this.state.depositedAmtState)
+        return (
+            <>
+                {console.log(this.state.depositedAmtState)}
+                <h1>can't be cahnged, only getter{bank.getAmtInBank()}</h1>
+                <hr />
+                <h1>getting here ={">"} {this.state?.depositedAmtState}</h1>
+                setting here ={">"} <input ref={inputRf} type="number"></input>
+                <button
+                    onClick={
+                        () => {
+                            bank.setDepositAmt(parseInt(inputRf.current.value))
+                            this.setState({ "depositedAmtState": bank.getDepositAmt() })
+                        }
+                    }
+                >
+                    setDeposit
+                </button>
+            </>
+        )
+    }
 }
 
 export default encapsulation;
