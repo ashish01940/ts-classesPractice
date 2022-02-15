279a45cb37cc87860c325d36acb2bebbcb79cbf2
ashish saini <smartsainiashish1@gmail.com>
2022-02-15 20:10:54 +0530

    polymorphism implemented using method overriding

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Comp1 } from './components/comp1';
 import RefComp1 from './components/forwarding Ref/refComp1';
 import RefComp2 from './components/forwarding Ref/refComp2';
 import Encapsulation from "./components/classesConcepts/encapsulation";
+import Polymorphism from './components/classesConcepts/polymorphism';
 
 interface a1 {
   login: "yes" | "no"
@@ -15,7 +16,8 @@ function App() {
     <div className="App">
       {/* {"yes" === val.login && <Comp1 name="ripper" num={54} tip={5} type="rich" />} */}
       {/* <RefComp1 /> */}
-      <Encapsulation />
+      {/* <Encapsulation /> */}
+      <Polymorphism a="d"/>
     </div>
   );
 }
--- src/components/classesConcepts/polymorphism.tsx
+++ src/components/classesConcepts/polymorphism.tsx
@@ -1,8 +1,41 @@
 import React, { Component } from 'react';
 
-export class polymorphism extends Component {
+class mainMethod {
+  protected confidentialInfo: string = " - I am jig saw, i wanna play a game";
+  getInfo(name: string) {
+    alert(name + this.confidentialInfo)
+  }
+}
+class mainMethod2 extends mainMethod {
+  getInfo(name: string) {
+    (name == "ashish") ? super.getInfo(name) : alert("not allowed");
+  }
+}
+
+interface polyPropsTypes {
+  a: string
+}
+interface polyStateTypes {
+  inputData: string,
+}
+export class polymorphism extends Component<polyPropsTypes, polyStateTypes> {
+  constructor(props: any) {
+    super(props)
+    this.state = {
+      inputData: "",
+    }
+  }
+  objChild = new mainMethod2();
   render() {
-    return <div></div>;
+    return <>
+    <h3>NOTE:Enter key also works to submit</h3>
+      <h1>only "ashish" can access using method overriding </h1>
+      <input
+        onKeyUp={(e: any) => { if (e.keyCode == 13) this.objChild.getInfo(this.state.inputData) }}
+        onChange={(e) => this.setState({ "inputData": e.target.value })}
+      ></input>
+      <button onClick={() => this.objChild.getInfo(this.state.inputData)}>send</button>
+    </>
   }
 }
 
