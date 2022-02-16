99d7998127442f3518ffd090842fe4943df5d384
ashish saini <smartsainiashish1@gmail.com>
2022-02-16 20:13:33 +0530

    abstraction is implemented

--- src/components/classesConcepts/Abstraction.tsx
+++ src/components/classesConcepts/Abstraction.tsx
@@ -1,9 +1,33 @@
-import React, { Component } from 'react';
+/* Abstraction is modeling or providing or implementing interfaces in classes so that
+it creates a seperation of duties in class  */
+/* for ex we r bound to use engine method in (mercedes and audi) and return type should be string but not in 
+sportscar  */
 
-export class Abstraction extends Component {
-  render() {
-    return <div></div>;
+export interface sportsCar {
+  engine(): string;
+}
+class car {
+  doors: number = 4;
+  constructor(doors: number) {
+    this.doors = doors;
   }
 }
 
-export default Abstraction;
+class mercedes extends car implements sportsCar {
+  constructor(doors: number) {
+    super(doors)
+  }
+  engine(): string {
+    return "this method is typed in interface"
+  }
+}
+
+class Audi extends mercedes {
+  constructor(doors: number) {
+    super(doors)
+  }
+  engine(): string {
+    return "this will automatically recieve the interface of mercedes"
+  }
+}
+
